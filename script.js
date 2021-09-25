ee08e12c40247c44beba4abd05e50f67a243d521
Harshit Paliwal <harshitpaliwal95@gmail.com>
2021-09-25 08:34:16 +0530

    small change

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const btnTranslate = document.getElementById('btn-translate');
 
 function callback() {
   console.log('clicked');
+  console.log('input', input.value);
 }
 btnTranslate.addEventListener('click', callback);
+
+const input = document.querySelector('#txt-input');
